4194bbc8d3461d5e028f924ab00a67f97b558e9e
Paulinagabriella <paulinagabriella2006@outlook.com>
2023-09-20 11:29:11 -0500

    this is lab12 commit

--- hackme.php
+++ hackme.php
@@ -5,14 +5,31 @@ $authorized = $_SESSION['authorized'];
 if ($authorized == 1)
 {
 
-echo "You are currently logged in <br> <br>";
-echo "If you want to log out click <a href='logout.php'> here </a> to log out";
+    echo "You are currently logged in <br> <br>";
+//NEW IF CONDITION for logged in users
+    $secret = $_POST['flag_value'];
+
+    if ($secret == 'blog')
+    {
+
+       echo "The flag is Flag-Success123";
+
+    }
+    else
+    {
+
+       echo "This webpage accepts a POST value of 'flag_value'. Use raft-small-words to brute-force this parameter and find the flag!";
+       echo "If you want to log out click <a href='logout.php'> here </a> to log out";
+
+    }
 
 }
-else
+if ($authorized != 1)
+
 {
 
-echo "You are not logged in. Please go to <a href='index.php'> this </a> to log in";
+       echo "You are not logged in. Please go to <a href='index.php'> this </a> to log in";
 
 }
+
 ?>
